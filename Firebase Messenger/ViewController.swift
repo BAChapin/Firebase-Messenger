4a5c9e3d8000bfd5e68c7b77f02ea7b6e04e8b17
Brett Chapin <blackroseangel@gmail.com>
2020-02-03 22:44:20 -0500

    Initial Commit

---
+++ Firebase Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Firebase Messenger
+//
+//  Created by Brett Chapin on 2/3/20.
+//  Copyright © 2020 Black Rose Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
